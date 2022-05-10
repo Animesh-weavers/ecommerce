074c29d409cffd53d81f05101ea1462003e37f57
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-10 18:13:21 +0530

    delete prev folder structure

---
+++ node_modules/react-slider/dist/es/prod/node_modules/@babel/runtime/helpers/esm/inheritsLoose.js
@@ -0,0 +1,1 @@
+import t from"./setPrototypeOf.js";function o(o,e){o.prototype=Object.create(e.prototype),o.prototype.constructor=o,t(o,e)}export{o as default};
---
+++ node_modules/react-slider/dist/es/prod/node_modules/@babel/runtime/helpers/esm/setPrototypeOf.js
@@ -0,0 +1,1 @@
+function t(e,o){return t=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t},t(e,o)}export{t as default};
